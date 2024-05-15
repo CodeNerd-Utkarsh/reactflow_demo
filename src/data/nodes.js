28cf722a1ebd846c96aa0bf1873a18ed7d057220
CodeNerd-Utkarsh <hbase_groups_official@aol.com>
2024-05-15 23:20:35 +0530

    added dummy nodes and dummy edges

---
+++ src/data/nodes.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        id: '1',
+        type: 'input',
+        data: { label: 'These are' },
+        position: { x: 0, y: 0 },
+    },
+
+    {
+        id: '2',
+        // you can also pass a React component as a label
+        data: { label: 'dummy nodes' },
+        position: { x: 30, y: 82 },
+    },
+
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,22 @@
-import ReactFlow, { Background, Controls } from 'reactflow';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
+import ReactFlow, { Background, Controls, MiniMap } from 'reactflow';
 import 'reactflow/dist/style.css';
+import initEdges from '../data/edges'
+import initNodes from '../data/nodes'
+
 function Home() {
+    const [nodes, setNodes] = useState(initNodes)
+    const [edges, setEdges] = useState(initEdges)
     return (
-        <div style={{ height: '100vh', width: '100vw' }}>
-            <ReactFlow>
+        <div style={{ height: '88vh', width: '90vw' }}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+            >
                 <Background />
                 <Controls />
+                <MiniMap />
             </ReactFlow>
         </div>
     )
